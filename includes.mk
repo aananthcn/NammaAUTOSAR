e4fb5caebf5157eb17913b4f80a9c083f1003b17
Aananth C N <c.n.aananth@gmail.com>
2022-12-24 00:41:47 +0530

    Link archive file list is generated written to path_defs.mk
    The uc_cgen.py script scans all makefile (child) and create a *.la file
    list by scanning the line with TARGET := definition.

--- includes.mk
+++
@@ -1,4 +0,0 @@
-# Inclusions
-include D:/_E/projects/oss/NammaAUTOSAR/submodules/MCAL/Mcu/src/bsp/startup/rp2040/rp2040.mk
-include D:/_E/projects/oss/NammaAUTOSAR/submodules/MCAL/Mcu/src/bsp/arch/arm/cortex-m0/cortex-m0.mk
-
